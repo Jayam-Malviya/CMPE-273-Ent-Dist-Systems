ab7220844bc07bbbb30be94ba5724752688dc777
Jayam-Malviya <jayam.malviya@sjsu.edu>
2016-09-09 13:53:08 -0700

    Updating main

--- RefresherAssignment/Java/Threading/Main.java
+++ RefresherAssignment/Java/Threading/Main.java
@@ -1,14 +1,12 @@
 package com.darkRealm;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> contentBuffer = new ArrayList<>();
-        Producer producer = new Producer(contentBuffer, 5);
-        Consumer consumer = new Consumer(contentBuffer);
-        producer.run();
-        consumer.run();
+
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer(producer);
+        producer.start();
+        consumer.start();
     }
 }
